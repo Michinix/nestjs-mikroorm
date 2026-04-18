18f1700c9a6c690266bab6749f90deb116090dda
0xMich <michel_marcotte@orange.fr>
2026-04-18 19:18:20 +0200

    FEAT: Ajout du module Product

---
+++ src/product/product.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/decorators/legacy';
+
+@Entity()
+export class Product {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+
+  @Property()
+  price!: number;
+
+  @Property()
+  description!: string;
+}
