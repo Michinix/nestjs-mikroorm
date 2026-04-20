1e289398997d140578fcee591078c3dcd3ba0b17
0xMich <michel_marcotte@orange.fr>
2026-04-20 15:24:55 +0200

    FIX: Correction de la méthode getHello en root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): { message: string } {
-    return this.appService.getHello();
+  root(): { message: string } {
+    return this.appService.root();
   }
 }
