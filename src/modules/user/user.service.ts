0cbe543dcafb6a55c1584f2779810362193eb239
Michinix <michel_marcotte@orange.fr>
2026-04-25 22:22:13 +0200

    FIX: Injection du repository et du service User

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { UserEntity } from './user.entity';
+import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: EntityRepository<UserEntity>,
+    private readonly em: EntityManager,
+  ) {}
+}
